65a76e769a4fdf0ee6afe04cfe076c1d070890e3
CLMcCork <37164206+CLMcCork@users.noreply.github.com>
2020-11-11 13:43:18 -0600

    added more pseudocode

--- location.js
+++ location.js
@@ -8,48 +8,54 @@ class Location {
         this.admin = admin; 
         this.supervisor = supervisor;
         this.barista = barista;
-        //do we need this one? 
         this.inventory = inventory; 
 
     } 
 
         getStoreLocation () {
-         //function to get store location 
-         //use an array: let stores = ["South Plaza", "Waldo", "Red Bridge"];
-         //if this.location === item in array, display that store's UI 
-        }
-     
-        addLocation () {
-         //function to add location
-         //use boolean, if admin, input info for new store, and add location
-         //else null 
-        }
-
-        deleteLocation () {
-         //function to delete location 
-         //use boolean, if admin, delete store
-         //else null 
-        }
-     
-        getGlobalInventory () {
-        //function to get global inventory list
-        //depends upon who is logging in 
-        //if user === this.admin || this.superervisor
-        //get global inventory list 
-        //else null 
-        }
-     
-        getMyStoreInventory () {
-         //function to get your store's inventory
-         //displayed w/ the store's UI 
-         //depending on which store you select
-         //displays that stores UI  
+            //function to get store location 
+            //use an array: let stores = ["South Plaza", "Waldo", "Red Bridge"];
+            //if this.location === item in array, display that store's UI 
         }
 
         displayStoreMenu () {
-         //function to display the working menu 
-         //if logged in as admin- see global menu
-         //if logged in as supervisor- see superviosr menu
-         //if logged in as barista- see barista menu 
+            //function to display each particular store's UI 
+            //depends on who is logged in: 
+            //if logged in as admin- see global menu (can see all store menus)
+            //if logged in as supervisor- see superviosr menu for your store
+            //if logged in as barista- see barista menu for your store 
         }
+        
+        getMyStoreInventory () {
+            //function to get your store's inventory
+            //displayed w/ the store's UI 
+            //depending on which store you select 
+            //displays that stores UI  
+        }
+     
+        addLocation () {
+            //function to add location
+            //if admin (use boolean)
+            //input info for new store
+            //add new location to current list of locations 
+            //show new location 
+            //else (not an admin) return "I'm sorry only admins can add locations."
+        }
+
+        deleteLocation () {
+            //function to delete location 
+            //if admin (use boolean) 
+            //delete store from list of current locations 
+            //else (not an admin) return "I'm sorry only admins can delete locations" 
+        }
+     
+        getGlobalInventory () {
+            //function to get global inventory list
+            //depends upon who is logging in 
+            //if user === this.admin || this.superervisor
+            //get global inventory list 
+            //else null 
+        }    
 }
+
+       
