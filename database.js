95abef140c9c0ec5e62ad90bfe01e7cb6e936958
mg-skc <71731493+mg-skc@users.noreply.github.com>
2020-12-01 16:54:16 -0600

    added Action Schema
    along with appropriate comments.

--- database.js
+++ database.js
@@ -42,14 +42,46 @@ Location = new Schema ({
 });
 
 Action = new Schema ({
-    user: User,
-    location: Location,
-    items: Goods,
-    time: Number,
-    panic: Boolean,
-    waste: Boolean,
-    recount?: Boolean?,
-    update permissions modify ???:
+    //date is required and will default to "now" if another date is not input
+    date: {type: Date, default: Date.now, required: true},
+    //user below is borrowing from User schema to ensure consistency
+    user: {type: Schema.type.Objectid, ref:User, required: true},
+    //item is similar to user except references Goods schema
+    item: {type:Schema.type.Objectid, ref:Goods},
+    //for the different "states" of items, using enumeration to limit the choices of answers for the string
+    itemState: { type: String, enum: ['waste', 'panic', 'for_sale', 'day_old'] },
+    //borrowing from location schema
+    location: {type: Schema.type.Objectid, ref:Location},
+    //if an item state is declared, a quantity will be input as a number
+    qty: Number,
+    //some actions are done by admins, and a type is registered with limited choices
+    adminAct:{ type: String, enum: ['add', 'delete', 'modify'] },
+    //these last two are for setting up new, delete, or modifying a user or location
+    //again borrowing from the proper schema for consistency
+    newUser: {type: Schema.type.Objectid, ref:User, required: true},
+    newLocation: {type: Schema.type.Objectid, ref:Location}
+    });
+
+    /*Regarding XML v JSON: here's an interesting article from StackOverflow
+    https://stackoverflow.com/questions/325085/when-to-prefer-json-over-xml
+    
+    Favor XML over JSON when any of these is true:
+
+    You need message validation
+    You're using XSLT
+    Your messages include a lot of marked-up text
+    You need to interoperate with environments that don't support JSON
+
+    Favor JSON over XML when all of these are true:
+
+    Messages don't need to be validated, or validating their deserialization is simple
+    You're not transforming messages, or transforming their deserialization is simple
+    Your messages are mostly data, not marked-up text
+    The messaging endpoints have good JSON tools
+
+    ***My comments: I think our use case, besides requiring JSON since Mongo and
+    mongoose, is that we don't have a serious amount of validation, no marked up
+    text to write to database, and our endpoints are JSON ready.  */
 
 })
 
