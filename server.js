e53b1c83b39d8d9faabd2742f4f31ca4d63872a7
anelson0112 <71732184+anelson0112@users.noreply.github.com>
2021-01-07 14:38:12 -0600

    added libraries and middleware

--- server.js
+++ server.js
@@ -3,17 +3,45 @@ const mongoose = require ("mongoose");
 //require body parser
 const bodyParser = require('body-parser');
 //call in schema models
-var Goods = require (".models.database.js");
-var Users = require (".models.database.js");
-var Locations = require (".models.database.js");
-
+var Goods = require ("./models.models.js");
+var Users = require ("./models.models.js");
+var Locations = require ("./models.models.js");
+//require express
+const express = require("express");
 //call in express
 const app = express();
+//add path library
+const path = require("path");
 
 //connect to Atlas cluster
+const mongoDB = "mongodb+srv://wastenotskilledkc:madANDal4life2021@wastenot1.sj0ff.mongodb.net/test"
 
 
+//accessing the connect method of mongoose
+//pass it the name of the DB cluster we have created
+//mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) => {
+    if(err) return console.error(err);
+    console.log('Connected to database');
+    });
+const db = mongoose.connection;
 
+//turns on the connection
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+//use the following middlware
+app.use(
+    //middleware for delivering static files
+    express.static(
+        //uses path library to take care of relative paths
+        path.join(__dirname, 'public')));
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+/* Here starts the API Calls*/
 
 //get a list of all the goods
 app.get("/goods", function (request, response){
